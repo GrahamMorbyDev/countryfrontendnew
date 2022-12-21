2655d7c9e30f754ab8dead96f148cfcd202329f5
Graham Morby <grahammorbydev@gmail.com>
2022-12-21 11:47:58 +0000

    history and about us page finished

---
+++ src/components/photosGrid.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="bg-cyan-900">
+      <div class="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        
+        <div class="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
+          <div class="group aspect-w-2 aspect-h-1 overflow-hidden rounded-lg sm:aspect-h-1 sm:aspect-w-1 sm:row-span-2">
+            <img src="@/assets/history/kiramac.jpg" alt="Kira Mac playing country on the coast 2019" class="object-cover object-center" />
+            <div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50" />
+            <div class="flex items-end p-6">
+              <div>
+                
+              </div>
+            </div>
+          </div>
+          <div class="group aspect-w-2 aspect-h-1 overflow-hidden rounded-lg sm:aspect-none sm:relative sm:h-full">
+            <img src="@/assets/history/elliot.jpg" alt="Elliot Jospeh playing Country on the coast" class="object-cover object-center sm:absolute sm:inset-0 sm:h-full sm:w-full" />
+            <div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0" />
+            <div class="flex items-end p-6 sm:absolute sm:inset-0">
+              <div>
+                
+                
+              </div>
+            </div>
+          </div>
+          <div class="group aspect-w-2 aspect-h-1 overflow-hidden rounded-lg sm:aspect-none sm:relative sm:h-full">
+            <img src="@/assets/history/rememberMondays.jpg" alt="Remember Mondays on stage at country on the coast" class="object-cover object-center sm:absolute sm:inset-0 sm:h-full sm:w-full" />
+            <div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0" />
+            <div class="flex items-end p-6 sm:absolute sm:inset-0">
+              <div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+  
+        <div class="mt-6 sm:hidden">
+          <a href="#" class="block text-sm font-semibold text-indigo-600 hover:text-indigo-500">
+            Browse all categories
+            <span aria-hidden="true"> &rarr;</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </template>
