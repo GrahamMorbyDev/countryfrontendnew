22c00f9ae19481d59255ce36f7de0d995ed79362
Jim <jim@enceladusdigital.com>
2023-01-06 22:18:46 +0000

    Edit blog text-editor

--- src/admin/editPost.vue
+++ src/admin/editPost.vue
@@ -194,19 +194,9 @@ export default {
                 toolbar: [ 
                     'heading', '|', 
                     'bold', 'italic', 'link', '|',
-                    'bulletedList', 'numberedList', 'decreaseIndent', 'increaseIndent', '|',
+                    'decreaseIndent', 'increaseIndent', '|',
                     'blockQuote', 'insertTable', 'undo', 'redo'
                 ],
-                heading: {
-                    options: [
-                        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-                        { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-                        { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
-                    ]
-                },
-                alignment: {
-                    options: [ 'left', 'right', 'center' ]
-                },
                 language: 'en'
             },
 
