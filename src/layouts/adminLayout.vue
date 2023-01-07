f370f249cbd3c071a5ab16b7a2305dc183ef77eb
Jim <jim@enceladusdigital.com>
2023-01-07 00:19:09 +0000

    highlight selected nav menu

--- src/layouts/adminLayout.vue
+++ src/layouts/adminLayout.vue
@@ -9,7 +9,11 @@
                 </div>
                 <div class="mt-5 flex-1 flex flex-col">
                     <nav class="flex-1 px-2 pb-4 space-y-1">
-                        <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-indigo-800 text-white' : 'text-indigo-100 hover:bg-indigo-600', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']">
+                        <a v-for="item in navigation" 
+                           :key="item.name" :href="item.href" 
+                           :class="[item.current ? 'bg-indigo-800 text-white' : 'text-indigo-100 hover:bg-indigo-600', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']"
+                           @click="clickNavOption(item.name)"
+                        >
                             <component :is="item.icon" class="mr-3 flex-shrink-0 h-6 w-6 text-indigo-300" aria-hidden="true" />
                             {{ item.name }}
                         </a>
@@ -18,7 +22,10 @@
 
                 <div class="mt-5 flex-1 flex flex-col">
                     <nav class="flex-1 px-2 pb-4 space-y-1">
-                        <a v-for="item in userNavigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-indigo-800 text-white' : 'text-indigo-100 hover:bg-indigo-600', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']">
+                        <a v-for="item in userNavigation" 
+                           :key="item.name" :href="item.href" 
+                           :class="[item.current ? 'bg-indigo-800 text-white' : 'text-indigo-100 hover:bg-indigo-600', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']"
+                        >
                             <component :is="item.icon" class="mr-3 flex-shrink-0 h-6 w-6 text-indigo-300" aria-hidden="true" />
                             {{ item.name }}
                         </a>
@@ -40,21 +47,21 @@
 <script>
 import { Dialog, DialogPanel, Menu, MenuButton, MenuItem, MenuItems, TransitionChild, 
          TransitionRoot } from '@headlessui/vue';
-import { BellIcon, CalendarIcon, ChartBarIcon, FolderIcon, HomeIcon, InboxIcon } from '@heroicons/vue/24/outline';
+import { ArrowLeftOnRectangleIcon, BuildingStorefrontIcon, HomeIcon, NewspaperIcon, UsersIcon } from '@heroicons/vue/24/outline';
 import { getStorage } from "@/saveStorage";
 export default {
     data() {
         return {
             navigation: [
-                { name: 'Home',      href: '/admin/home',    icon: HomeIcon,  current: true },
-                { name: 'Users',     href: '/admin/users',   icon: HomeIcon,  current: false },
-                { name: 'Order',     href: '/admin/orders',  icon: HomeIcon,  current: false },
-                { name: 'Blog',      href: '/admin/posts',   icon: HomeIcon,  current: false },
+                { name: 'Home',      href: '/admin/home',    icon: HomeIcon,                  current: false },
+                { name: 'Users',     href: '/admin/users',   icon: UsersIcon,                 current: false },
+                { name: 'Order',     href: '/admin/orders',  icon: BuildingStorefrontIcon,    current: false },
+                { name: 'Blog',      href: '/admin/posts',   icon: NewspaperIcon,             current: false },
             ],
             userNavigation: [
                 //{ name: 'Your Profile', href: '#' },
                 //{ name: 'Settings', href: '#' },
-                { name: 'Sign out',  href: '/admin/logout',  icon: HomeIcon,  current: false },
+                { name: 'Sign out',  href: '/admin/logout',  icon: ArrowLeftOnRectangleIcon,  current: false },
             ],
             sidebarOpen: false,
             active: false,
@@ -79,6 +86,15 @@ export default {
 
     created() {
         this.checkUser();
+
+        // Set nav menu selected
+        this.navigation.map(function(i) { 
+            i.current = false; 
+            if (i.href == window.location.pathname) {
+                i.current = true; 
+            }
+            return i;
+        });
     }
 }
 </script>
