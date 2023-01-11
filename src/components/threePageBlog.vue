b8f12c206ad992dbe8e456aa0a64287db1a76760
Graham Morby <grahammorbydev@gmail.com>
2023-01-11 15:59:57 +0000

    three page blogs now connecting to api

--- src/components/threePageBlog.vue
+++ src/components/threePageBlog.vue
@@ -10,14 +10,14 @@
         <div class="mx-auto mt-12 grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3">
           <div v-for="post in posts" :key="post.title" class="flex flex-col overflow-hidden rounded-lg shadow-lg">
             <div class="flex-shrink-0">
-              <img class="h-48 w-full object-cover" :src="post.imageUrl" alt="" />
+              <img class="h-48 w-full object-cover" :src="post.image_url" alt="" />
             </div>
             <div class="flex flex-1 flex-col justify-between bg-white p-6">
               <div class="flex-1">
                 <p class="text-sm font-medium text-indigo-600">
-                  <a :href="post.category.href" class="hover:underline">{{ post.category.name }}</a>
+                  
                 </p>
-                <a :href="post.href" class="mt-2 block">
+                <a href="#" class="mt-2 block">
                   <p class="text-xl font-semibold text-gray-900">{{ post.title }}</p>
                   <p class="mt-3 text-base text-gray-500">{{ post.description }}</p>
                 </a>
@@ -25,18 +25,16 @@
               <div class="mt-6 flex items-center">
                 <div class="flex-shrink-0">
                   <a :href="post.author.href">
-                    <span class="sr-only">{{ post.author.name }}</span>
-                    <img class="h-10 w-10 rounded-full" :src="post.author.imageUrl" alt="" />
+                    <span class="sr-only">{{ post.author }}</span>
                   </a>
                 </div>
                 <div class="ml-3">
                   <p class="text-sm font-medium text-gray-900">
-                    <a :href="post.author.href" class="hover:underline">{{ post.author.name }}</a>
                   </p>
                   <div class="flex space-x-1 text-sm text-gray-500">
-                    <time :datetime="post.datetime">{{ post.date }}</time>
+                    <time :datetime="post.datetime">{{ post.created_at }}</time>
                     <span aria-hidden="true">&middot;</span>
-                    <span>{{ post.readingTime }} read</span>
+                    <span>{{ post.mins_read }} minutes read</span>
                   </div>
                 </div>
               </div>
@@ -47,61 +45,25 @@
     </div>
   </template>
   
-  <script setup>
-  const posts = [
-    {
-      title: 'Boost your conversion rate',
-      href: '#',
-      category: { name: 'Article', href: '#' },
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
-      date: 'Mar 16, 2020',
-      datetime: '2020-03-16',
-      imageUrl:
-        'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-      readingTime: '6 min',
-      author: {
-        name: 'Roel Aufderehar',
-        href: '#',
-        imageUrl:
-          'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
+  <script>
+  import axios from "axios";
+  export default {
+    data() {
+        return {
+            api_url:  process.env.VUE_APP_API_URL,
+            posts: []
+        };
     },
-    {
-      title: 'How to use search engine optimization to drive sales',
-      href: '#',
-      category: { name: 'Video', href: '#' },
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.',
-      date: 'Mar 10, 2020',
-      datetime: '2020-03-10',
-      imageUrl:
-        'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-      readingTime: '4 min',
-      author: {
-        name: 'Brenna Goyette',
-        href: '#',
-        imageUrl:
-          'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
+    created() {
+      this.getPosts();
     },
-    {
-      title: 'Improve your customer experience',
-      href: '#',
-      category: { name: 'Case Study', href: '#' },
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
-      date: 'Feb 12, 2020',
-      datetime: '2020-02-12',
-      imageUrl:
-        'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-      readingTime: '11 min',
-      author: {
-        name: 'Daniela Metz',
-        href: '#',
-        imageUrl:
-          'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-    },
-  ]
-  </script>
+    methods: { 
+      getPosts() {
+        axios.get(this.api_url + 'api/blog?per_page=3', this.formData)
+            .then(response => {
+                this.posts = response.data.records
+            })
+      }
+    }
+  }
+</script>
